36cf7b1053346c00f4af3c56d517d325177755ef
Daniel Esteban Nombela <dani@hellacoders.com>
2014-01-05 03:16:57 -0600

    v0.0.3 - Multiple editor tabs per window

--- js/frappeditor.js
+++ js/frappeditor.js
@@ -1,3 +1,29 @@
+LIB = {
+	arraySearch : function(haystack, needle, index, returnIndex) {		
+		try {
+			haystack.forEach(function(item, i) {
+				if(item && item[index] === needle) {
+					throw returnIndex ? parseInt(i, 10) : item;
+				}
+			});
+		} catch(r) {
+			return r;
+		}
+		return false;
+	},
+	fileId : function(file) {
+		var escape = function(str) {
+				return str.replace(/\\/g, '_').replace(/\//g, '_').replace(/\./g, '_');
+			};
+
+		return file.name ? escape(file.path) + '_' + escape(file.name) : (new Date()).getTime();
+	},
+	cancelHandler : function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+	}
+};
+
 EDITOR = {
 	init : function(params) {
 		var themelist = ace.require('ace/ext/themelist').themesByName;
@@ -6,16 +32,7 @@ EDITOR = {
 			version : FRAPP.version.frapp,
 			year : (new Date()).getFullYear()
 		}));
-		this.ace = ace.edit('editor');
-		this.setTheme(FRAPP.storage.get('theme', true) || 'twilight');
-		this.ace.getSession().setMode('ace/mode/javascript');
-		this.ace.getSession().setUseWrapMode(true);
-		this.ace.getSession().setUseSoftTabs(false);
-		this.ace.on('change', function() {
-			EDITOR.unsaved = true;
-			$('footer .status').text(L.notSaved + (EDITOR.file ? ': ' + EDITOR.file.name : ''));
-		});
-
+		
 		$('.modal#open').on('show.bs.modal', function() {
 			var modal = $(this);
 			EDITOR.renderFileList(EDITOR.file ? EDITOR.file.path : null, $('.fileList', modal), {
@@ -31,11 +48,25 @@ EDITOR = {
 		});
 
 		$('.modal#newItem form').submit(function(e) {
-			e.stopPropagation();
-			e.preventDefault();
 			var modal = $(this).parents('.modal').first(),
-				name = e.target.name.value;
+				name = e.target.name.value,
+				rename = function() {
+					var file = EDITOR.file,
+						tab = $('#tabs #tab' + file.id),
+						editor = $('#editor #editor' + file.id),
+						a = $('#tabs #tab' + file.id + ' a')[0];
+
+					a.removeChild(a.firstChild);
+					a.insertBefore(document.createTextNode(name), a.firstChild);
+					file.name = name;
+					file.id = LIB.fileId(file);
+					tab.attr('id', 'tab' + file.id);
+					editor.attr('id', 'editor' + file.id);
+					EDITOR.open(file);
+					file.editor.getSession().setMode(ace.require('ace/ext/modelist').getModeForPath(file.name).mode);
+				};
 			
+			LIB.cancelHandler(e);
 			if(name === '') return;
 			switch(e.target.type.value) {
 				case 'directory':
@@ -45,12 +76,10 @@ EDITOR = {
 					});
 				break;
 				case 'file':
-					EDITOR.file = {
-						path : EDITOR.path,
-						name : name
-					};
+					EDITOR.file.path = EDITOR.path || '.';
+					rename();
 					EDITOR.save(function() {
-						EDITOR.open(EDITOR.file);
+						EDITOR.updateTree(EDITOR.path);
 						modal.modal('hide');
 					});
 				break;
@@ -59,7 +88,7 @@ EDITOR = {
 						oldName = e.target.oldName.value;
 
 					FRAPP.rename(path, oldName, name, function() {
-						EDITOR.file && EDITOR.file.path === path && EDITOR.file.name === oldName && (EDITOR.file.name = name) && EDITOR.open(EDITOR.file);
+						EDITOR.file && EDITOR.file.path === path && EDITOR.file.name === oldName && rename();
 						EDITOR.updateTree(EDITOR.path);
 						modal.modal('hide');
 					});
@@ -101,7 +130,7 @@ EDITOR = {
 							if(item.type === 'directory') FRAPP.rmdir(item.fullName, cb);
 							else {
 								FRAPP.unlink(item.path, item.name, cb);
-								EDITOR.file && EDITOR.file.path === item.path && EDITOR.file.name === item.name && EDITOR.newFile(true);
+								EDITOR.file && EDITOR.file.path === item.path && EDITOR.file.name === item.name && EDITOR.close();
 							}
 						}
 					}
@@ -112,7 +141,7 @@ EDITOR = {
 	},
 	listDirectory : function(path, callback) {
 		FRAPP.listDirectory(path, function(data) {
-			var ignore = ['.DS_Store'],
+			var ignore = ['.DS_Store', '.git'],
 				items = [];
 
 			data.forEach(function(item) {
@@ -145,31 +174,87 @@ EDITOR = {
 		});
 	},
 	open : function(file) {
-		var modelist = ace.require('ace/ext/modelist'),
-			editor = this.ace;
+		!(file.id) && (file.id = LIB.fileId(file));
+		if(this.file && this.file.id === file.id) return;
+		!this.files && (this.files = []);
+		
+		var self = this,
+			container = $('#editor'),
+			tabs = $('#tabs'),
+			already = LIB.arraySearch(this.files, file.id, 'id'),
+			render = function(contents) {
+				var li = $('<li id="tab' + file.id + '"><a></a></li>'),
+					div = $('<div id="editor' + file.id + '" class="editor" style="display:none"></div>'),
+					editor = ace.edit(div[0]),
+					modelist = ace.require('ace/ext/modelist'),
+					session = ace.createEditSession(contents, file.name ? modelist.getModeForPath(file.name).mode : 'ace/mode/javascript');
+				
+				session.setUseWrapMode(true);
+				session.setUseSoftTabs(false);
+				editor.setTheme('ace/theme/' + (FRAPP.storage.get('theme', true) || 'twilight'));
+				editor.setSession(session);
+				editor.on('change', function() {
+					file.unsaved = true;
+					li.addClass('unsaved');
+					$('footer .status').text(L.notSaved + (file.name ? ': ' + file.name : ''));
+				});
+				file.editor = editor;
+				$('a', li).text(file.name || L.newFile).click(function() {
+					self.open(file);
+				}).append($('<span>&times;</span>').click(function() {
+					self.close(file);
+				}));
+				tabs.append(li);
+				container.append(div);
+				self.files.push(self.file = file);
+				show();
+			},
+			show = function() {
+				container.children().hide();
+				$('#editor' + file.id, container).show();
+				tabs.children().removeClass('active');
+				$('#tab' + file.id, tabs).addClass('active');
+				$('footer .status').text(file.name ? L.editing + (file.name ? ': ' + file.name : '') : L.newFile);
+				FRAPP.setTitle(file.name ? file.name + ' ─ ' + file.path : L.newFile);
+				self.updateTree(file.path || self.path);
+				file.editor.focus();
+			};
 
-		if(EDITOR.unsaved && !confirm(L.unsavedConfirmation)) return;
-		this.file = file;
-		this.updateTree(file.path);
-		FRAPP.readFile(file.path, file.name, function(contents) {
-			editor.setValue(contents);
-			editor.clearSelection();
-			editor.gotoLine(0);
-			editor.session.setMode(modelist.getModeForPath(file.name).mode);
-			$('footer .status').text(L.editing + ': ' + file.name);
-			FRAPP.setTitle(file.name + ' ─ ' + file.path);
-			delete EDITOR.unsaved;
-		});
+		if(already) {
+			this.file = file = already;
+			return show();
+		}
+		if(!file.name) return render('');
+		FRAPP.readFile(file.path, file.name, render);
+	},
+	close : function(file) {
+		file = file || this.file;
+		if(!file) return window.close();
+		if(file.unsaved && !confirm(L.unsavedConfirmation)) return;
+		var index = LIB.arraySearch(this.files, file.id, 'id', true);
+		this.files[index].editor.destroy();
+		this.files.splice(index, 1);
+		$('#tabs #tab' + file.id).remove();
+		$('#editor #editor' + file.id).remove();
+		if(this.file.id !== file.id) return;
+		delete this.file;
+		$('footer .status').text('');
+		FRAPP.setTitle('FrappEditor');
+		this.updateTree(this.path);
+		this.files.length && this.open(this.files[index - (index > 0 ? 1 : 0)]);
 	},
 	save : function(callback) {
-		if(EDITOR.saving) return;
-		if(!this.file) return this.saveAs();
-		var file = this.file;
-		EDITOR.saving = true;
-		FRAPP.saveFile(file.path, file.name, this.ace.getValue(), function() {
+		if(!this.file || this.saving) return;
+		if(!this.file.name) return this.saveAs();
+		var file = this.file,
+			self = this;
+		
+		this.saving = true;
+		FRAPP.saveFile(file.path, file.name, file.editor.getValue(), function() {
+			delete self.file.unsaved;
+			delete self.saving;
 			$('footer .status').text(L.saved + ': ' + file.name);
-			delete EDITOR.unsaved;
-			delete EDITOR.saving;
+			$('#tabs #tab' + file.id).removeClass('unsaved');
 			callback && callback();
 		});
 	},
@@ -180,11 +265,8 @@ EDITOR = {
 		$('input[name="type"]', modal).val('file');
 		modal.modal('show');
 	},
-	newFile : function(force) {
-		if(!force && EDITOR.unsaved && !confirm(L.unsavedConfirmation)) return;
-		delete this.file;
-		this.ace.setValue('');
-		$('footer .status').text('');
+	newFile : function() {
+		this.open({});
 	},
 	newFolder : function() {
 		var modal = $('.modal#newItem');
@@ -195,10 +277,14 @@ EDITOR = {
 	},
 	setTheme : function(name) {
 		FRAPP.storage.set('theme', name, true);
-		this.ace.setTheme('ace/theme/' + name);
+		if(!this.files) return;
+		this.files.forEach(function(f) {
+			f.editor.setTheme('ace/theme/' + name);
+		});
 	},
 	showSearch : function(replace) {
-		ace.require('ace/ext/searchbox').Search(this.ace, replace);
+		if(!this.file) return;
+		ace.require('ace/ext/searchbox').Search(this.file.editor, replace);
 	},
 	newWindow : function() {
 		FRAPP.load({
@@ -207,6 +293,12 @@ EDITOR = {
 				url : 'https://github.com/danielesteban/FrappEditor.git'
 			}
 		}, {path : EDITOR.path});
+	},
+	undo : function() {
+		this.file && this.file.editor.undo();
+	},
+	redo : function() {
+		this.file && this.file.editor.redo();
 	}
 }
 
@@ -223,12 +315,16 @@ window.addEventListener('frapp.init', function(e) {
 
 	/* Keyboard shorcuts */
 	$(window).keydown(function(e) {
+		if((e.metaKey || e.ctrlKey) && e.altKey && (e.keyCode === 78 || e.keyCode === 192)) return EDITOR.newFolder();
+		if((e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode === 78) return EDITOR.newWindow();
 		(e.metaKey || e.ctrlKey) && e.keyCode === 78 && EDITOR.newFile();
-		(e.metaKey || e.ctrlKey) && e.altKey && (e.keyCode === 78 || e.keyCode === 192) && EDITOR.newFolder();
-		(e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode === 78 && EDITOR.newWindow();
+		if((e.metaKey || e.ctrlKey) && e.altKey && e.keyCode === 83) return EDITOR.saveAs();
 		(e.metaKey || e.ctrlKey) && e.keyCode === 83 && EDITOR.save();
-		(e.metaKey || e.ctrlKey) && e.altKey && e.keyCode === 83 && EDITOR.saveAs();
 		(e.metaKey || e.ctrlKey) && e.keyCode === 79 && $('.modal#open').modal('show');
+		if((e.metaKey || e.ctrlKey) && e.keyCode === 87) {
+			LIB.cancelHandler(e);
+			EDITOR.close();
+		}
 	});
 
 	/* Init editor */
